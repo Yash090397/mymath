4c49bc1d9d75dba4a6c7edabb0c7de91f25f8006
Yash090397 <naharys15.comp@coep.ac.in>
2016-09-29 20:54:40 +0530

    Add files via upload

---
+++ mymath.h
@@ -0,0 +1,30 @@
+//#include <stdio.h>
+#include "mymath.c"
+#define MINIMUM 0.0000000000001
+double acos(double x) //-- Compute arc cosine of x.
+double asin(double x) //-- Compute arc sine of x.
+double atan(double x) //-- Compute arc tangent of x.
+double atan2(double y, double x) //-- Compute arc tangent of y/x, using the signs of both arguments to determine the quadrant of the return value.
+double ceil(double x) //-- Get smallest integral value that exceeds x.
+double cos(double x) //-- Compute cosine of angle in radians.
+double cosh(double x) //-- Compute the hyperbolic cosine of x.
+div_t div(int number, int denom) //-- Divide one integer by another.
+double exp(double x) //-- Compute exponential of x
+double fabs (double x ) //-- Compute absolute value of x.
+double floor(double x) //-- Get largest integral value less than x.
+double fmod(double x, double y) //-- Divide x by y with integral quotient and return remainder.
+double frexp(double x, int *expptr) //-- Breaks down x into mantissa and exponent of no.
+labs(long n) //-- Find absolute value of long integer n.
+double ldexp(double x, int exp) //-- Reconstructs x out of mantissa and exponent of two.
+ldiv_t ldiv(long number, long denom) //-- Divide one long integer by another.
+double log(double x) //-- Compute log(x).
+double log10 (double x ) //-- Compute log to the base 10 of x.
+double modf(double x, double *intptr) //-- Breaks x into fractional and integer parts.
+double pow(double x, double y) //-- Compute x raised to the power y.
+double sin(double x) //-- Compute sine of angle in radians.
+double sinh(double x) //-- Compute the hyperbolic sine of x.
+double sqrt(double x) //-- Compute the square root of x.
+void srand(unsigned seed) //-- Set a new seed for the random number generator (rand).
+double tan(double x) //-- Compute tangent of angle in radians.
+double tanh(double x) //-- Compute the hyperbolic tangent of x.
+
